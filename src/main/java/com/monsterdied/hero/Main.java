782686b14095e6f6c77e0246204860edd0ac8e91
Tomás Sarmento <up202108778@fe.up.pt>
2022-09-29 17:25:15 +0100

    second commit

--- src/main/java/com/monsterdied/hero/Main.java
+++ src/main/java/com/monsterdied/hero/Main.java
@@ -1,7 +1,18 @@
 package com.monsterdied.hero;
 
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null); // we don't need a cursor
+        screen.startScreen(); // screens must be started
+        screen.doResizeIfNecessary(); // resize screen if necessary
     }
 }
