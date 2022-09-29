b60b4f4e0bbc6c61b7c29b0f649700f10ccecfdb
Tomás Sarmento <up202108778@fe.up.pt>
2022-09-29 17:28:25 +0100

    3 commit

--- src/main/java/com/monsterdied/hero/Main.java
+++ src/main/java/com/monsterdied/hero/Main.java
@@ -8,11 +8,16 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        Screen screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null); // we don't need a cursor
-        screen.startScreen(); // screens must be started
-        screen.doResizeIfNecessary(); // resize screen if necessary
+    public static void main(String[] args) {
+        try {
+            Terminal terminal = new
+                    DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
